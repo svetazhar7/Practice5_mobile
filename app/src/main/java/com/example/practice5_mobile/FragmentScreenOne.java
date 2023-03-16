44e3122dc4d0db4f68a6500d3223f006f2fbdb74
svetazhar7 <svetazhar7@gmail.com>
2023-03-16 20:19:40 +0300

    Реализованы переходы между фрагментами с помощью библиотеки AndroidX Navigation

--- app/src/main/java/com/example/practice5_mobile/FragmentScreenOne.java
+++ app/src/main/java/com/example/practice5_mobile/FragmentScreenOne.java
@@ -1,10 +1,47 @@
 package com.example.practice5_mobile;
 
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.practice5_mobile.databinding.Screen1Binding;
 
 public class FragmentScreenOne extends Fragment {
+    Screen1Binding binding;
     public FragmentScreenOne()
     {
         super(R.layout.screen1);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = Screen1Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_first_fragment_to_second_fragment);
+            }
+        });
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_first_fragment_to_third_fragment);
+            }
+        });
+
     }
 }
